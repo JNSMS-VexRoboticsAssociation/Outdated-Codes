b9174903eeef8651e32940903b3df1c145d4bc69
AngelicaRoot <1423634265@qq.com>
2021-03-17 12:46:31 +0800

    Created Branch

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -5,7 +5,7 @@ using signature = vision::signature;
 using code = vision::code;
 
 // A global instance of brain used for printing to the V5 Brain screen
-brain  Brain;
+brain Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
@@ -18,9 +18,9 @@ motor Motor6 = motor(PORT6, ratio18_1, true);
 motor Motor7 = motor(PORT7, ratio36_1, false);
 motor Motor8 = motor(PORT8, ratio36_1, false);
 /*vex-vision-config:begin*/
-signature Vision15__SIG_RED = signature (1, 1455, 6721, 4088, -1499, -217, -858, 0.7, 0);
-signature Vision15__SIG_BLUE = signature (2, -3933, -1665, -2799, 3959, 10019, 6989, 0.8, 0);
-vision Vision15 = vision (PORT15, 150, Vision15__SIG_RED, Vision15__SIG_BLUE);
+signature Vision15__SIG_RED = signature(1, 1455, 6721, 4088, -1499, -217, -858, 0.7, 0);
+signature Vision15__SIG_BLUE = signature(2, -3933, -1665, -2799, 3959, 10019, 6989, 0.8, 0);
+vision Vision15 = vision(PORT15, 150, Vision15__SIG_RED, Vision15__SIG_BLUE);
 /*vex-vision-config:end*/
 inertial Inertial = inertial(PORT20);
 
@@ -33,6 +33,6 @@ inertial Inertial = inertial(PORT20);
  * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit( void ) {
-  // nothing to initialize
+void vexcodeInit(void) {
+    // nothing to initialize
 }
